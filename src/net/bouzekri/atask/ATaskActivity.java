439fd68f6558ab28dfc141b6053e47e76c02d38d
jbouzekri <jonathan.bouzekri@gmail.com>
2012-07-14 16:45:41 +0200

    Init android project

---
+++ src/net/bouzekri/atask/ATaskActivity.java
@@ -0,0 +1,24 @@
+package net.bouzekri.atask;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class ATaskActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    
+}
