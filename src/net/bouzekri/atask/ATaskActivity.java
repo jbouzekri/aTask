ccf25b9db2e79aee7b865b295c6c4cb79b9f8f7a
jbouzekri <jonathan.bouzekri@gmail.com>
2012-07-15 16:00:33 +0200

    list view and adapter

--- src/net/bouzekri/atask/ATaskActivity.java
+++ src/net/bouzekri/atask/ATaskActivity.java
@@ -1,22 +1,53 @@
 package net.bouzekri.atask;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class ATaskActivity extends Activity {
 
 	public static final int TASK_FORM = 1;
 	
+	private ListView mTasksList;
+	private List<Task> mTasks;
+	
+	private TaskDatabaseHelper taskDBHelper;
+	private TaskListAdapter adapter;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        setTaskDBHelper(new TaskDatabaseHelper(this));
+        
         setContentView(R.layout.main);
+        
+        mTasksList = (ListView) findViewById(R.id.next_task_list);
+        mTasksList.setEmptyView(findViewById(R.id.no_task));
+        mTasks = loadNextTasks();
+        
+        adapter = new TaskListAdapter(getApplicationContext());
+        adapter.setListItems(mTasks);
+        mTasksList.setAdapter(adapter);
     }
 
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	mTasks = loadNextTasks();
+    	adapter.setListItems(mTasks);
+    	adapter.notifyDataSetChanged();
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
@@ -51,5 +82,37 @@ public class ATaskActivity extends Activity {
             default:
                 break;
         }
+    }
+    
+    public List<Task> loadNextTasks() {
+    	List<Task> tasks = new ArrayList<Task>();
+    	
+    	SQLiteDatabase sqliteDatabase = getTaskDBHelper().getReadableDatabase();
+    	Cursor cursor = sqliteDatabase.query(TaskDatabaseHelper.TASK_TABLE_NAME, 
+    										 new String[] {TaskDatabaseHelper.TASK_TABLE_TITLE, TaskDatabaseHelper.TASK_TABLE_CONTENT}, 
+										 	null, null, null, null, null);
+    	if(cursor.moveToFirst())
+        {
+    		Task newTask;
+            do
+            {
+            	newTask = new Task();
+                newTask.setTitle(cursor.getString(0));
+                newTask.setContent(cursor.getString(1));
+                tasks.add(newTask);
+            } while (cursor.moveToNext());
+            if(cursor != null && !cursor.isClosed())
+                cursor.close();
+        }
+    	
+    	return tasks;
+    }
+    
+    public TaskDatabaseHelper getTaskDBHelper() {
+        return taskDBHelper;
+    }
+
+    public void setTaskDBHelper(TaskDatabaseHelper taskDBHelper) {
+        this.taskDBHelper = taskDBHelper;
     }
 }
---
+++ src/net/bouzekri/atask/TaskListAdapter.java
@@ -0,0 +1,63 @@
+package net.bouzekri.atask;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class TaskListAdapter extends BaseAdapter {
+
+	private LayoutInflater mInflater;
+	private List<Task> mTasks;
+	
+	public TaskListAdapter(Context context) {
+		mInflater = LayoutInflater.from(context);
+	}
+	
+	@Override
+	public int getCount() {
+		return mTasks.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mTasks.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		AppViewHolder holder;
+		if (convertView == null) {
+			convertView = mInflater.inflate(R.layout.task_row, null);
+			
+			holder = new AppViewHolder();
+			holder.mTitle = (TextView) convertView.findViewById(R.id.task_row_title);
+			convertView.setTag(holder);
+		} else {
+			holder = (AppViewHolder) convertView.getTag();
+		}
+		holder.setTitle(mTasks.get(position).getTitle());
+		return convertView;
+	}
+
+	public void setListItems(List<Task> list) {
+		mTasks = list;
+	}
+	
+	public class AppViewHolder {
+		private TextView mTitle;
+		
+		public void setTitle(String title) {
+			mTitle.setText(title);
+		}
+	}
+}
