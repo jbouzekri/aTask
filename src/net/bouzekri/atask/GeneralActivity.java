fe3cedae946b07aab758be5f589e18ca5e625db1
jbouzekri <jonathan.bouzekri@gmail.com>
2012-08-24 21:56:30 +0200

    add licence header

--- src/net/bouzekri/atask/GeneralActivity.java
+++ src/net/bouzekri/atask/GeneralActivity.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2012 Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
 package net.bouzekri.atask;
 
 import java.util.ArrayList;
@@ -23,8 +39,12 @@ public class GeneralActivity extends Activity {
         
         SQLiteDatabase sqliteDatabase = getTaskDBHelper().getReadableDatabase();
         Cursor cursor = sqliteDatabase.query(TaskDatabaseHelper.TABLE_NAME, 
-                                             new String[] {TaskDatabaseHelper.COLUMN_ID,TaskDatabaseHelper.COLUMN_TITLE, TaskDatabaseHelper.COLUMN_CONTENT}, 
-                                             null, null, null, null, null);
+                                             new String[] {
+                                                 TaskDatabaseHelper.COLUMN_ID,
+                                                 TaskDatabaseHelper.COLUMN_TITLE, 
+                                                 TaskDatabaseHelper.COLUMN_CONTENT, 
+                                                 TaskDatabaseHelper.COLUMN_DATE
+                                             },  null, null, null, null, null);
         if(cursor.moveToFirst())
         {
             Task newTask;
@@ -34,6 +54,7 @@ public class GeneralActivity extends Activity {
                 newTask.setId(cursor.getInt(0));
                 newTask.setTitle(cursor.getString(1));
                 newTask.setContent(cursor.getString(2));
+                newTask.setDate(cursor.getLong(3));
                 tasks.add(newTask);
             } while (cursor.moveToNext());
             if(cursor != null && !cursor.isClosed())
